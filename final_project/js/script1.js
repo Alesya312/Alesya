35724832b8358a24292ec7861dee0aaec755057b
Alesya <alesyadaranchuk@yandex.by>
2023-08-24 11:43:45 +0300

    burger

--- final_project/js/script1.js
+++ final_project/js/script1.js
@@ -2,13 +2,13 @@ $(document).ready(function () {
     $(".open").addClass('active')
 
     $(".open").on("click", function () {
-        $("#menu").css("right", 0)
+        $("#menu").css("display", "block")
         $(this).removeClass("active");
         $(".close").addClass("active");
     })
 
     $(".close").on("click", function () {
-        $("#menu").css("right", "-375px")
+        $("#menu").css("display", "none")
         $(this).removeClass("active");
         $(".open").addClass("active");
     })
